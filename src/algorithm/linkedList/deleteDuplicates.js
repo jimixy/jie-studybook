0c23d5fd149b37e8bf295a63aefdad527b309b6a
wangjinjie <wangjinjie@skieer.com>
2021-11-30 15:34:55 +0800

    chore: 删除链表中的重复元素；

--- src/algorithm/linkedList/deleteDuplicates.js
+++ src/algorithm/linkedList/deleteDuplicates.js
@@ -24,7 +24,7 @@ function deleteDuplicates(head) {
   if (!head) {
     return head;
   }
-  const dummy = new ListNode(0, head);
+  const dummy = new ListNode(-1, head);
   let cur = dummy;
   while (cur.next && cur.next.next) {
     if (cur.next.val === cur.next.next.val) {
@@ -38,3 +38,61 @@ function deleteDuplicates(head) {
   }
   return dummy.next;
 }
+
+function deleteDuplicates2(head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  let fakeNode = new ListNode(-1, head);
+  let cur = fakeNode;
+  let hasDuplicate = false;
+  while (cur) {
+    if (cur.next && cur.next.next && cur.next.val == cur.next.next.val) {
+      cur.next = cur.next.next;
+      hasDuplicate = true;
+    } else if (hasDuplicate) {
+      cur.next = cur.next.next;
+      hasDuplicate = false;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return fakeNode.next;
+}
+
+/** 测试代码 */
+class ListNode {
+  constructor(val, next) {
+    this.val = val;
+    this.next = next || null;
+  }
+}
+class LinkedList {
+  val;
+  next;
+  constructor(arr) {
+    if (arr == null || arr.length == 0) {
+      throw new Error("arr is null or empty");
+    }
+    this.val = arr[0];
+    let cur = this;
+    for (let i = 1; i < arr.length; i++) {
+      cur.next = new ListNode(arr[i]);
+      cur = cur.next;
+    }
+  }
+  toString() {
+    let cur = this;
+    let str = "";
+    while (cur != null) {
+      str += cur.val + "->";
+      cur = cur.next;
+    }
+    return str;
+  }
+}
+
+const arr = [0, 1, 1, 2, 3, 3, 4, 4, 5];
+const list = new LinkedList(arr);
+console.log(list.toString());
+console.log(deleteDuplicates2(list));

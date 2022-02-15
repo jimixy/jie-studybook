3968c574d40e8a4e40d99cf27ace0cc6a1255470
wangjinjie <wangjinjie@skieer.com>
2022-02-15 10:35:53 +0800

    chore: 优化;

--- src/algorithm/graph/solveNQueens.js
+++ src/algorithm/graph/solveNQueens.js
@@ -24,21 +24,22 @@ function solveNQueens(n) {
    * 1. 判断列是否有Q
    * 2. 判断左斜上方是否有Q
    * 3. 判断右斜上方是否有Q
+   * 因为row会逐行增大，所有只要判断row之前的行是否有相交的Q
    */
   const isValid = (arr, row, col) => {
-    // 1
+    // 判断1
     for (let i = 0; i < row; i++) {
       if (arr[i][col] === "Q") {
         return false;
       }
     }
-    // 2
+    // 判断2
     for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
       if (arr[i][j] === "Q") {
         return false;
       }
     }
-    // 3
+    // 判断3
     const n = arr[row].length;
     for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
       if (arr[i][j] === "Q") {
@@ -50,6 +51,3 @@ function solveNQueens(n) {
   dfs(arr, 0);
   return res;
 }
-
-const result = solveNQueens(4);
-console.log(result);

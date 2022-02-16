52a0960b7dc8f6e5a3ccae0ddb8b31df5df33805
wangjinjie <wangjinjie@skieer.com>
2022-02-16 11:57:39 +0800

    feat: 二叉树的堂兄弟节点;

---
+++ src/algorithm/binaryTree/isCousins.js
@@ -0,0 +1,31 @@
+/**
+ * 993. 二叉树的堂兄弟节点
+ * https://leetcode-cn.com/problems/cousins-in-binary-tree/
+ */
+
+function isCousins(root, x, y) {
+  const queue = [root];
+  while (queue.length) {
+    const len = queue.length;
+    let sum = x + y;
+    for (let i = 0; i < len; i++) {
+      const { left, right, val } = queue.shift();
+      // 同一个父节点下的
+      if (
+        left &&
+        right &&
+        ((left.val === x && right.val === y) ||
+          (left.val === y && right.val === x))
+      ) {
+        return false;
+      }
+      if (val === x || val === y) {
+        sum -= val;
+      }
+      if (sum === 0) return true;
+      left && queue.push(left);
+      right && queue.push(right);
+    }
+  }
+  return false;
+}

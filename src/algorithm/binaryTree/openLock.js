b16d78d93ebdda121fec41ec567c3ebe47625811
jimi <2310385247@qq.com>
2022-02-18 23:26:25 +0800

    chore: 转盘锁优化；

--- src/algorithm/binaryTree/openLock.js
+++ src/algorithm/binaryTree/openLock.js
@@ -5,7 +5,9 @@
 
 function openLock(deadends, target) {
   const queue = ["0000"];
-  const visited = ["0000"];
+  const visited = new Set(["0000"]);
+  // 使用set来优化性能
+  const deadSet = new Set(deadends);
   let step = 0;
   while (queue.length) {
     const len = queue.length;
@@ -16,53 +18,26 @@ function openLock(deadends, target) {
         return step;
       }
       // 死亡数字
-      if (deadends.includes(node)) {
+      if (deadSet.has(node)) {
         continue;
       }
       for (let j = 0; j < 4; j++) {
-        const newNodeStr = plusNum(node, j);
-        if (!visited.includes(newNodeStr)) {
-          visited.push(newNodeStr);
-          queue.push(newNodeStr);
+        const num = Number(node[j]);
+        const up = (num + 1) % 10;
+        const upStr = node.substring(0, j) + up + node.substring(j + 1);
+        if (!visited.has(upStr)) {
+          visited.add(upStr);
+          queue.push(upStr);
         }
-        const newNodeStr2 = minusNum(node, j);
-        if (!visited.includes(newNodeStr2)) {
-          visited.push(newNodeStr2);
-          queue.push(newNodeStr2);
+        const down = (num + 9) % 10;
+        const downStr = node.substring(0, j) + down + node.substring(j + 1);
+        if (!visited.has(downStr)) {
+          visited.add(downStr);
+          queue.push(downStr);
         }
       }
     }
     step++;
   }
-
-  function plusNum(str, index) {
-    const newNode = str.split("");
-    if (newNode[index] === "9") {
-      newNode[index] = "0";
-    } else {
-      newNode[index] = parseInt(newNode[index]) + 1;
-    }
-    return newNode.join("");
-  }
-
-  function minusNum(str, index) {
-    const newNode = str.split("");
-    if (newNode[index] === "0") {
-      newNode[index] = "9";
-    } else {
-      newNode[index] = parseInt(newNode[index]) - 1;
-    }
-    return newNode.join("");
-  }
-
   return -1;
 }
-
-const result = openLock(["0201", "0101", "0102", "1212", "2002"], "0202");
-console.log(result);
-
-// const result2 = openLock(
-//   ["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"],
-//   "8888"
-// );
-// console.log(result2);
